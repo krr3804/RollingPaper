b07b8f95192215ac3ffcb03b83d744439631a3aa
Yuri JI <45191069+ur2e@users.noreply.github.com>
2022-09-18 20:36:10 +0900

    Update data.sql

--- rollingpaper/src/main/resources/data.sql
+++ rollingpaper/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `paper`.`paper` (`id`, `content`, `nickname`, `password`) VALUES ('1', '1', '1', '1');
+INSERT INTO `paperdb`.`paper` (`id`, `content`, `nickname`, `password`) VALUES ('1', '1', '1', '1');
